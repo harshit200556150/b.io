b863d407422f7d9266edb133467f221f6ce17155
harshit200556150 <156473415+harshit200556150@users.noreply.github.com>
2024-04-14 12:18:49 -0400

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function searchWeather() {
         return;
     }
 
-    const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY'; // Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
